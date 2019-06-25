93d0a0eb6a5c35ad8b82f368c5251d9690c508f9
Lucy Veron <lucymveron@gmail.com>
2019-06-25 22:12:14 +0200

    second person sprite

--- resources/tileset.tsx
+++ resources/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="tileset" tilewidth="16" tileheight="16" tilecount="25" columns="5">
- <image source="tileset.png" width="80" height="80"/>
+<tileset version="1.2" tiledversion="1.2.3" name="tileset" tilewidth="16" tileheight="16" tilecount="50" columns="10">
+ <image source="tileset.png" width="160" height="80"/>
 </tileset>
